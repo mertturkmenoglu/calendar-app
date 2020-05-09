baca6640de84085294aada5c8b07aacb9d3ce4c1
Mert <mertturkmenoglu99@gmail.com>
2020-05-09 20:52:15 +0300

    Initial commit

---
+++ app/src/main/java/ce/yildiz/calendarapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ce.yildiz.calendarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
