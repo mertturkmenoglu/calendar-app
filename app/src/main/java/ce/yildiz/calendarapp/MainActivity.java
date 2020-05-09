6c770d9a1d1765527c843d1f267acb788380d40f
Mert <mertturkmenoglu99@gmail.com>
2020-05-09 21:22:46 +0300

    Add CalendarView

--- app/src/main/java/ce/yildiz/calendarapp/MainActivity.java
+++ app/src/main/java/ce/yildiz/calendarapp/MainActivity.java
@@ -1,14 +1,29 @@
 package ce.yildiz.calendarapp;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import ce.yildiz.calendarapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
+        setContentView(root);
+
+        binding.calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(MainActivity.this, "Date: " + dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
