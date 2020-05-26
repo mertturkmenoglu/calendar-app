4c23aa174fa84209bd2bc68aa310fdfa2429c000
Mert <mertturkmenoglu99@gmail.com>
2020-05-26 17:47:59 +0300

    CA-29 Reformat classes
    Reformat Validate.java class

--- app/src/main/java/ce/yildiz/calendarapp/util/Validate.java
+++ app/src/main/java/ce/yildiz/calendarapp/util/Validate.java
@@ -5,14 +5,6 @@ import android.text.TextUtils;
 import androidx.annotation.NonNull;
 
 public final class Validate {
-    public static final class Date {
-        public static boolean validateFields(int year, int monthOfYear, int dayOfMonth) {
-            return year != Constants.DATE_DEFAULT_YEAR
-                    && monthOfYear != Constants.DATE_DEFAULT_MONTH
-                    && dayOfMonth != Constants.DATE_DEFAULT_DAY;
-        }
-    }
-
     public static boolean validateEmail(@NonNull String email) {
         return !TextUtils.isEmpty(email);
     }
