2f5a844d354bd005fca1a2ab7cff3b2263ed5e21
Mert <mertturkmenoglu99@gmail.com>
2020-05-26 17:33:07 +0300

    CA-29 Reformat classes
    Reformat LoginActivity.java class and add email and password validation
    methods to Validate.java class

--- app/src/main/java/ce/yildiz/calendarapp/util/Validate.java
+++ app/src/main/java/ce/yildiz/calendarapp/util/Validate.java
@@ -1,5 +1,9 @@
 package ce.yildiz.calendarapp.util;
 
+import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
+
 public final class Validate {
     public static final class Date {
         public static boolean validateFields(int year, int monthOfYear, int dayOfMonth) {
@@ -7,5 +11,13 @@ public final class Validate {
                     && monthOfYear != Constants.DATE_DEFAULT_MONTH
                     && dayOfMonth != Constants.DATE_DEFAULT_DAY;
         }
+    }
+
+    public static boolean validateEmail(@NonNull String email) {
+        return !TextUtils.isEmpty(email);
+    }
+
+    public static boolean validatePassword(@NonNull String password) {
+        return !TextUtils.isEmpty(password) && !(password.length() < Constants.MIN_PASSWORD_LENGTH);
     }
 }
